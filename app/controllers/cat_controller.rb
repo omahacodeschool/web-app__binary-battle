8febc7420ca2ada3c8ee6a1b31e57e779efb9657
Beth Haubert <bethany.haubert@gmail.com>
2016-02-16 11:09:25 -0600

    added winner/loser

--- app/controllers/cat_controller.rb
+++ app/controllers/cat_controller.rb
@@ -1,6 +1,19 @@
 MyApp.get "/" do
-	@cat_list = Cat.all
+	@cat_1, @cat_2 = Cat.all.sample(2)
   erb :"home"
+end
+
+MyApp.post "/cat_like/:winner/:loser>" do
+	@winner = Cat.find(params[:winner])
+	@loser = Cat.find(params[:loser])
+	@cat.likes += 1
+	@cat.save
+  erb :"like_added/#{@winner.id}/#{@loser.id}"
+end
+
+MyApp.get "/like_added/:winner/:loser" do
+	@winner = Cat.find(params[:winner])
+	@loser = Cat.find(params[:loser])
 end
 
 MyApp.get "/admin" do
@@ -11,12 +24,12 @@ end
 MyApp.post "/add_cat" do
 	@cat = Cat.new
 	@cat.name = params[:name]
-	@cat.votes = 0
+	@cat.likes = 0
 	@cat.save
   redirect :"admin"
 end
 
-MyApp.post "cat_delete/:cat_id" do
+MyApp.post "/cat_delete/:cat_id" do
 	@cat = Cat.find(params[:cat_id])
 	@cat.delete
   redirect :"admin"
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,4 @@
 class Cat < ActiveRecord::Base
+	
 end
+
