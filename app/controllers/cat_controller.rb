2907eed42cceb2fb6e1f0e8f3075700e32b7eb61
Beth Haubert <bethany.haubert@gmail.com>
2016-02-16 10:44:17 -0600

    admin page

--- app/controllers/cat_controller.rb
+++ app/controllers/cat_controller.rb
@@ -9,9 +9,15 @@ MyApp.get "/admin" do
 end
 
 MyApp.post "/add_cat" do
-	@cat =  Cat.new
+	@cat = Cat.new
 	@cat.name = params[:name]
 	@cat.votes = 0
 	@cat.save
+  redirect :"admin"
+end
+
+MyApp.post "cat_delete/:cat_id" do
+	@cat = Cat.find(params[:cat_id])
+	@cat.delete
   redirect :"admin"
 end
