014b7f2234c6dfdc37933e5bb90cffe9be48ccc8
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-18 10:07:41 -0600

    added categories table, model, controller, views  for bonus work. added more view pages. dropped battles table after reconsidering things. a match is an algorithmic thing that happens to entries. I store the outcome in results. I actually don't store any data in battles. so it isn't necessary. especially with categories

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -1,6 +1,6 @@
 require_relative "./_configure"
 
-DB.define_table("entries")
+
 DB.define_column("entries", "name", "string")
 DB.define_column("entries", "bio", "text")
 DB.define_column("entries", "image", "string")
@@ -8,11 +8,17 @@ DB.define_column("entries", "thumbnail", "string")
 DB.define_column("entries", "battle_id", "integer")
 DB.define_column("entries", "retired", "boolean")
 
+
 DB.define_table("battles")
 DB.define_column("battles", "name", "string")
 DB.define_column("battles", "description", "text")
 DB.define_column("battles", "image", "string")
-DB.define_column("battles", "locked", "boolean")
+DB.define_column("battles", "category_id", "integer")
+
+DB.define_table("categories")
+DB.define_column("categories", "name", "string")
+DB.define_column("categories", "locked", "boolean")
+
 
 DB.define_table("results")
 DB.define_column("results", "score", "integer")
