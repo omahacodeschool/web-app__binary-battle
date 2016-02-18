9acccd32d3e59578cc674c492091358a9ee65666
Chris Foral <chris.foral@gmail.com>
2016-02-18 12:18:26 -0600

    Added update and delete functionality. Yet to be tested

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,30 @@ MyApp.post "/add_movie_form" do
   @movie.save
 
   erb :"admin/add_movie"
-end 
+end
+
+MyApp.get "/delete_movie/:movie_id" do
+  @movies = Movie.all
+  @movie  = Movie.find_by_id(params[:movie_id])
+  
+  @movie.delete
+    
+  erb :"admin/add_movies"
+end
+
+MyApp.get "/update_movie/:movie_id" do
+  @movies = Movie.all
+  @movie  = Movie.find_by_id(params[:movie_id])
+
+  erb :"admin/update_movie"
+end
+
+MyApp.post "/process_movie_update/:movie_id" do
+  @movies = Movie.all
+  @movie  = Movie.find_by_id(params[:movie_id])
+
+  @movie.movie_title = (params["movie_title_update"])
+  @movie.save
+
+  erb :"admin/add_movies"
+end
