0eeb7aacbaba710c5876cf4cb096f7bf1abc2933
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-18 09:09:50 -0600

    defined schema. getting folders set up

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -1,1 +1,19 @@
 require_relative "./_configure"
+
+DB.define_table("entries")
+DB.define_column("entries", "name", "string")
+DB.define_column("entries", "bio", "text")
+DB.define_column("entries", "image", "string")
+DB.define_column("entries", "retired", "boolean")
+
+DB.define_table("battles")
+DB.define_column("battles", "name", "string")
+DB.define_column("battles", "description", "text")
+DB.define_column("battles", "image", "string")
+DB.define_column("battles", "entry_id", "integer")
+DB.define_column("battles", "locked", "boolean")
+
+DB.define_table("results")
+DB.define_column("results", "score", "integer")
+DB.define_column("results", "entry_id", "integer")
+DB.define_column("results", "battle_id", "integer")
