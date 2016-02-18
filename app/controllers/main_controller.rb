4ab43636520b29ae9d9b078a8b828d88a0e4c2c0
Shannon Jackson <shannonfromomaha@gmail.com>
2016-02-18 11:56:38 -0600

    1156

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 MyApp.get "/" do
   characters = Character.all
+  #chooses 2 random characters
   @char1 = characters.sample
   @char2 = characters.sample
+  #if it's the same character twice, picks 2 more
   if @char1.id == @char2.id
     redirect to ("/")
   else
@@ -12,15 +14,26 @@ MyApp.get "/" do
 end
 
 MyApp.post "/vote" do
+  #saves the winner and loser to the battle table
   b = Battle.new
   b.winner_id = params[:winner]
   b.loser_id = params[:loser]
   b.save
   @winner = Character.find_by_id(params[:winner])
   @loser = Character.find_by_id(params[:loser])
-  binding.pry
   erb :"votesuccess"
+end
 
+MyApp.get "/ranks" do
+  #finds the best to worst character
+  characters = Character.all
+  characters.each do |character|
+    character.rank
+  end
+  binding.pry
+  #returns an array 
+  @winner = arr[0][0]
+  erb :"ranks"
 end
 
 #DB.define_table("characters")
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,27 +1,30 @@
 class Character < ActiveRecord::Base
   def how_many_wins
     x = self.id
-    wins = Battle.where({winner_id => x})
-    return wins.length
+    wins = Battle.where({"winner_id" => x})
+    if wins == nil
+      return 0
+    else
+      return wins.length
+    end
   end
 
   def how_many_losses
     x = self.id
-    losses = Battle.where({loser_id => x})
+    losses = Battle.where({"loser_id" => x})
+    if losses == nil
+      return 0
+    else
     return losses.length
+    end
   end
 
-  def self.ranks
-    allcharacters = Character.all
-    characterhash = Hash.new
-      allcharacters.each do |character|
-        x = character.how_many_wins
-        y = character.how_many_wins + character.how_many_losses
-        score = x / y
-        characterhash[character.id] = score
-      end
-    characterarray = characterhash.sorty_by{|k,v|v}
-    return characterarray
+  def rank
+    #a method to sort an array of characters
+    x = self.how_many_wins.to_f
+    y = self.how_many_wins.to_f + self.how_many_losses.to_f
+    score = x / y
+    return score
   end
 
 end
