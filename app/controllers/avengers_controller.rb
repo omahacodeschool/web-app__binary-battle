ca56bdda88ff1e55eb45c877447ea6e5c54e0c9f
Ryan Newell <youcatastrophe@gmail.com>
2016-02-18 16:26:12 -0600

    Beginning

---
+++ app/controllers/avengers_controller.rb
@@ -0,0 +1,3 @@
+class Avenger < ActiveRecord::Base
+
+end 
--- db/define_schema.rb
+++ db/define_schema.rb
@@ -1,1 +1,5 @@
 require_relative "./_configure"
+
+DB.define_table("avengers")
+DB.define_column("avengers", "name", "string")
+DB.define_column("avengers", "votes", "integer")
