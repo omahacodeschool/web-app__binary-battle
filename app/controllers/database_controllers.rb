9de4277e7ae9562ae431bfe8e431fbe43a55783d
Kyle Sandage <ksandage08@gmail.com>
2016-02-18 13:51:59 -0600

    buildings added

--- app/controllers/database_controllers.rb
+++ app/controllers/database_controllers.rb
@@ -13,7 +13,23 @@ MyApp.post "/add_vote/:id" do
   @w = Movie.find_by_id(params[:id])
   @w.increment!(:vote_count)
   @w.save
-
 #Put cool stuff here.
 erb :"/movies/movie_voted"
+end
+
+#Add a Building to DB
+MyApp.post "/add_building" do
+  b = Building.new
+  b.building_name = params["input_building"]
+  b.building_image = params["input_url"]
+  b.vote_count = 0
+  b.save
+  erb :"/buildings/building_added"
+end
+
+#Increment the chosen building's vote count by 1
+MyApp.post "/building_vote/:id" do
+  @w = Building.find_by_id(params[:id])
+  @w.increment!(:vote_count)
+  @w.save
 end
