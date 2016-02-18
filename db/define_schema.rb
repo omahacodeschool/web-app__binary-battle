e98ad8975ff5040ed26716d690050692a95a24ef
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-18 10:55:20 -0600

    thought about it. changed things around with the schema a bit more. --delayed push as I was helping my peers

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -5,9 +5,8 @@ DB.define_column("entries", "name", "string")
 DB.define_column("entries", "bio", "text")
 DB.define_column("entries", "image", "string")
 DB.define_column("entries", "thumbnail", "string")
-DB.define_column("entries", "catgegory_id", "integer")
+DB.define_column("entries", "category_id", "integer")
 DB.define_column("entries", "retired", "boolean")
-
 
 DB.define_table("categories")
 DB.define_column("categories", "name", "string")
@@ -17,6 +16,7 @@ DB.define_column("categories", "locked", "boolean")
 
 
 DB.define_table("results")
-DB.define_column("results", "score", "integer")
-DB.define_column("results", "entry_id", "integer")
+DB.define_column("results", "winner_id", "integer")
+DB.define_column("results", "loser_id", "integer")
 DB.define_column("results", "category_id", "integer")
+
