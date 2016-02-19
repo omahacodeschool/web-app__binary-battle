0b2b530e8bb187642816385513ae1ea1400dc839
Chris Foral <chris.foral@gmail.com>
2016-02-19 15:40:44 -0600

    More stat methods defined and implemented

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -14,14 +14,27 @@ class Movie < ActiveRecord::Base
     x.count
   end
 
-  def movie_button_1
-    x = Vote.where({"vote_button" => self.id})
-    x.count
+  def movie_button_1_up
+    x = Vote.where({"vote_button" => 1})
+    y = x.where({"upvote_id" => self.id})
+    y.count
   end
 
-  def movie_button_2
-    x = Vote.where({"vote_button" => self.id})
-    x.count
+  def movie_button_2_up
+    x = Vote.where({"vote_button" => 2})
+    y = x.where({"upvote_id" => self.id})
+    y.count
   end
 
+  def movie_button_1_down
+    x = Vote.where({"vote_button" => 1})
+    y = x.where({"downvote_id" => self.id})
+    y.count
+  end
+
+  def movie_button_2_down
+    x = Vote.where({"vote_button" => 2})
+    y = x.where({"downvote_id" => self.id})
+    y.count
+  end
 end
