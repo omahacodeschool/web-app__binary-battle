3fe07c4dd5ac5918a9340761117859d40ed957a6
Amy Wall <amymariewall@gmail.com>
2016-02-19 14:13:13 -0600

    Working on ranking page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -19,18 +19,6 @@ end
 MyApp.get "/competitor/vote" do
   competitors = Competitor.all
   @match_competitors = competitors.sample(2)
-  @match_competitors_ids = []
-    @match_competitors.each do |c|
-      @match_competitors_ids << c.id
-    end
-
-  #matchup = Matchup.new
-  #Get two numbers randomly that are some combination possible out of 
-  #all competitor ids
-  #matchup.first_competitor_id = #first number of random matchup number
-  #matchup.second_competitor_id = #second number of random matchup number
-  #don't forget to write access_competitor method so you can display competitor
-  #names on page
   erb :"competitor/vote"
 end
 
@@ -45,22 +33,14 @@ MyApp.post "/vote/create/:winner_id/:loser_id" do
   erb :"confirm_submission"
 end
 
+MyApp.get "/vote/view/" do
+  competitors = Competitor.all
+  
+  #get all the matchups where one competitor is the winner
+  #get all the matchups where that same competitor is the loser
+  # divide the number of matchups where they are the winner by the number of matchups where they appeared at all
 
-# MyApp.post "/vote/create/:comp_1_id/:comp_2_id" do
-#   matchup = Matchup.new
-#   loser_object_id = []
-#   matchup.winner_competitor_id = params["winner_id"]
-#   competitors = Competitor.where({"id" => [params[:comp_1_id], params[:comp_2_id]]})
-#   loser_object = competitors.where.not({"id" => params["winner_id"]})
-# loser_object.each do |c|
-#   loser_object_id << c.id
-# end
-#   matchup.loser_competitor_id = loser_object_id[0]
-#   binding.pry
-#   matchup.save
-#   @confirm_message = "Successfully voted for #{matchup.winner_name} over #{matchup.loser_name}!"
-#   erb :"confirm_submission"
-# end
+end
 
 
 
--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -1,3 +1,41 @@
 class Competitor < ActiveRecord::Base
 
+  #Gets the number of times a single competitor has won
+  #Returns a float of wins
+
+  def get_win_count
+    wins = Matchup.where({"winner_competitor_id" => self.id})
+    win_count = wins.count
+    return win_count.to_f
+  end
+
+  #Gets the number of times a single competitor has lost
+  #Returns a float of losses
+
+  def get_loss_count
+    losses = Matchup.where({"loser_competitor_id" => self.id})
+    loss_count = losses.count
+    return loss_count.to_f
+  end
+
+  def rank
+    competitors = Competitor.all
+    winner_with_win_percent = {}
+    competitors.each do |c|
+      win_count = c.get_win_count
+      loss_count = c.get_loss_count
+      win_percent = (win_count/loss_count)
+      #store a hash with the competitor object as key and win_percent as value?
+      #is wanting to create a hash a good indicator that I should create 
+      #another column in my table?
+      winner_with_win_percent[c] = win_percent
+    end
+    return winner_with_win_percent.sort_by {|key, value| value }
+
+
+  end
+
+
+
+
 end
