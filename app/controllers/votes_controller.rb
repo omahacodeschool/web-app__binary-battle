aad9282da1a28da74a0a189a38af36c35771ee75
Chris Foral <chris.foral@gmail.com>
2016-02-18 22:51:51 -0600

    Form working. Passes both winner and loser IDs to votes_controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,16 +1,17 @@
 # This controller action collects the the user votes and places the results accordingly
+# How will it know what the other option is that wasn't selected?
 MyApp.post "/user_vote" do
   @votes = Vote.all
   @vote  = Vote.new
 
-  @vote.upvote_id   = 
-  @vote.downvote_id =
+  @vote.upvote_id   = (params["upvote_id"])
+  @vote.downvote_id = (params["downvote_id"])
   @vote.save
 
-  erb :"user/result"
+  erb :"user/vote_result"
 end
 
-MyApp.get "/result" do
+MyApp.get "/vote_result" do
 
 
   erb :"user/result"
