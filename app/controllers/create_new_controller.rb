71a384797aa35c108ba0639721c521fda6d13625
Shannon Jackson <shannonfromomaha@gmail.com>
2016-02-19 11:34:13 -0600

    ooooh that's an interesting little bug isn't it

--- app/controllers/create_new_controller.rb
+++ app/controllers/create_new_controller.rb
@@ -36,4 +36,4 @@ end
 #
 #DB.define_table("battles")
 #DB.define_column("battles","winner_id","integer")
-#DB.define_column("battles","loser_id","integer")
+#DB.define_column("battles","loser_id","integer") 
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,15 +8,11 @@ end
 MyApp.get "/battle/:id" do
   @universe = Universe.find_by_id(params[:id])
   characters = Character.where({"universe_id" => @universe.id})
-  #chooses 2 random characters
-  @char1 = characters.sample
-  @char2 = characters.sample
-  #if it's the same character twice, picks 2 more
-  if @char1.id == @char2.id
-    redirect to ("/battle/:id")
-  else
-    erb :"battle"
-  end
+  characters = characters.shuffle
+  @char1 = characters.pop
+  @char2 = characters.pop
+
+  erb :"battle"
 end
 
 MyApp.post "/vote" do
@@ -27,19 +23,21 @@ MyApp.post "/vote" do
   b.save
   @winner = Character.find_by_id(params[:winner])
   @loser = Character.find_by_id(params[:loser])
+  @universe = Universe.find_by_id(params[:universe])
   erb :"votesuccess"
 end
 
-MyApp.get "/ranks" do
+MyApp.get "/ranks/:id" do
   #finds the best to worst character
-  characters = Character.all
+  @universe = Universe.find_by_id(params[:id])
+  characters = Character.where({"universe_id" => (params[:id])})
   ranks = Hash.new
   
   characters.each do |character|
     x = character.rank
     ranks[character.character_name] = x
   end
-  
+  binding.pry
   @arr = ranks.sort{|a1,a2| a2[1]<=>a1[1]}
   #returns an array, sorted most to least
   #@winner is the name of the #1 spot
@@ -57,5 +55,5 @@ end
 #DB.define_column("battles","loser_id","integer")
 #
 #
-#DB.define_table("universes")
+#DB.define_table("universes") 
 #DB.define_column("universes","universe_name","string")
