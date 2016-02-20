d61b65831ff9396110a2b774df7b64eccbf203b1
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-20 10:46:19 -0600

    can vote and go to 'vote_added' page.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,21 @@
-MyApp.post "/vote_added/:winner/:loser/:order" do
+MyApp.post "/vote_added/:winner/:loser" do
   @winner = Legend.find(params[:winner])
   @loser = Legend.find(params[:loser])
   @vote = Vote.new
   @vote.winner_id = @winner.id
   @vote.loser_id = @loser.id
-  @vote.order = params[:order]
+  # @vote.order = params[:order]
   @vote.save
-  redirect :"vote_added/#{@winner_id}/#{@loser_id}"
+  # binding.pry
+  # redirect :"vote_added/#{@winner_id}/#{@loser_id}"
+  erb :"votes/vote_added"
 end
 
-MyApp.get "/votes/:winner/:loser" do
-  @winner = Legend.find(params[:winner])
-  @loser = Legend.find(params[:loser])
-  erb :"votes/vote_added"  
-end
+# MyApp.get "/votes/:winner/:loser" do
+#   @winner = Legend.find(params[:winner])
+#   @loser = Legend.find(params[:loser])
+#   erb :"votes/vote_added"  
+# end
 
 MyApp.get "/results" do
   erb :"votes/results"
