c43aaf842a746ce88aa29a24038ff5ce0ca3b4cb
Kyle Sandage <ksandage08@gmail.com>
2016-02-21 16:47:03 -0600

    This isn't working, switching my ranking system

--- app/controllers/flow_controllers.rb
+++ app/controllers/flow_controllers.rb
@@ -22,10 +22,11 @@ MyApp.get "/show_movies" do
   @sorted = x.winning_movie
   duplicates = @sorted
   arr = duplicates.group_by{|k, v| v}
-  v = arr.select{|k,v| v.count > 1}.values.flatten(2)
+  v = arr.select{|k,v| v.count > 1}
+  binding.pry
   v.each do |i|
-    tie_movies = i[0]
-   binding.pry
+    i.values.flatten(2)
+ binding.pry
  end
   erb :"/movies/display_movies"
 end
