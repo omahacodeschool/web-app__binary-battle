e8efd89f332a9cc5ee515b35b9c50bb4bf267534
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-20 23:33:41 -0600

    having some issues with an undefined method in my voting battle

--- app/controllers/pool_controller.rb
+++ app/controllers/pool_controller.rb
@@ -6,6 +6,19 @@ end
 
 MyApp.post "/battles/battle/:id/play" do
   @category = Category.find_by_id(params[:id])
-  @pool = @category.get_pool
-  erb :"/battles/view_battle"
+  @pool = Pool.where({"category_id" => @category.id}).sample(2)
+  @nominee_ids = @pool.get_selection_ids
+  erb :"/battles/battle/#{@category.id}/play"
+end
+
+
+MyApp.post "/battles/battle/:id/voted" do
+  @category = Category.find_by_id(params[:id])
+  @nominees_results = Result.new
+  @nominees_results.lost_id =
+  @nominees_results.loved_id =
+  @nominees_results.category_id = @category.id
+  @nominees_results.locked = true
+  @nominees_results.save
+  redirect :"/battles/battle/#{@category.id}/play"
 end
--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,12 +1,21 @@
 class Pool < ActiveRecord::Base
-  # RETURNS String containing error mesage when attempting to add new category that matches prexisting one
-  def get_already_exists_error
-    return "That Battle already exists. To add this battle, first close the active instance, or simply edit your preexisting battle if there are not yet any votes for it."
-  end
+    # RETURNS String containing error mesage when attempting to add new category that matches prexisting one
+    def get_already_exists_error
+      return "That Battle already exists. To add this battle, first close the active instance, or simply edit your preexisting battle if there are not yet any votes for it."
+    end
 
-  def get_nominee_info
-    nominee = self.nominee_id
-    return Nominee.find_by_id(nominee)
-  end
+    def get_nominee_info
+      nominee = self.nominee_id
+      return Nominee.find_by_id(nominee)
+    end
+
+    def get_selection_ids
+      arr = []
+      selections = self.all
+      selections.each do |selection|
+        arr << selection.nominee_id
+      end
+      return arr
+    end
   
-end
+  end
