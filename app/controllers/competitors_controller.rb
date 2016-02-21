e6182a5986ab6f25cbc95dfcda5d0ee657f8790f
Thorin Butler <thorin.butler@gmail.com>
2016-02-20 20:15:41 -0600

    Sketching out what I want to have happen.  So far, I'm just trying to make it make sense and add in the things I think would be needed.

--- app/controllers/competitors_controller.rb
+++ app/controllers/competitors_controller.rb
@@ -10,3 +10,13 @@ MyApp.post "/competitor_processed" do
   @y = [x.name, x.type]
   erb :"competitors/processed"
 end
+
+MyApp.get "/all_competitors" do 
+  @competitors = Competitor.all
+  erb :"competitors/competitors_list"
+end 
+
+MyApp.get "/view_competitor/:competitor_id" do 
+  @competitor = Competitor.find_by_id(params[:competitor_id])
+  erb :"competitors/single_competitor"
+end
