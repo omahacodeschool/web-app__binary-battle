7c3c93677980800540a334ba30256e15606761bc
Thorin Butler <thorin.butler@gmail.com>
2016-02-20 19:28:04 -0600

    Set up the DB schema.  Two tables one for competitors and one for results.  I might need more than this when all is said and done, but starting simple here.  Also, set up ActiveRecord models for both of the tables.  Next going to try and set up the forms for adding competitors.

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -2,7 +2,14 @@ require_relative "./_configure"
 
 # Define your tables and their columns below this line.
 # -----------------------------------------------------------------------------
+DB.define_table("competitors")
+DB.define_column("competitors", "name", "string")
+DB.define_column("competitors", "type", "string")
 
+DB.define_table("results")
+DB.define_column("results","comp_id_1","integer")
+DB.define_column("results","comp_id_2","integer")
+DB.define_column("results","winner","integer")
 
 # Column names should be all lowercase and have no spaces or any other special
 # characters--except for underscores. So "running_time" is valid, but
