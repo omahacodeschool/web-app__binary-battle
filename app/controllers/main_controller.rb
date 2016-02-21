64014b0154b03588ef53a9ed0a20d27773d0b2c6
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-21 17:13:29 -0600

    Choice bias super ghetto but functional

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,9 +3,14 @@ MyApp.get "/" do
   @new_showdown = Showdown.new
   @new_showdown.choice_one = @display_planets[0]
   @new_showdown.choice_two = @display_planets[1]
-  @new_showdown.frequency_one = 
-  @new_showdown.frequency_two = 
   @new_showdown.save
+  @id_of_new_showdown = @new_showdown.id
+  @id_of_last_showdown = @id_of_new_showdown - 1
+  @last_showdown = Showdown.find_by_id(@id_of_last_showdown)
+  @new_showdown.frequency_one = @last_showdown.frequency_one
+  @new_showdown.frequency_two = @last_showdown.frequency_two
+  @new_showdown.save
+  binding.pry
   erb :"main/welcome"
 end
 
@@ -29,7 +34,6 @@ MyApp.get "/choice_selected/:num" do
     @new_score_of_freq_two = @current_score_of_freq_two + 1
     @last_showdown.frequency_two = @new_score_of_freq_two
     @last_showdown.save
-    binding.pry
   end
   erb :"main/choice_selected"
 end
