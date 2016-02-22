b4cd3332ff5e931e1f0a694dda95a2ed03b6f018
Tray Harrington <traylon.harrington@gmail.com>
2016-02-22 17:29:55 -0600

    Attempting to get static form to work

--- app/controllers/user_controllers.rb
+++ app/controllers/user_controllers.rb
@@ -1,3 +1,12 @@
 MyApp.get "/add_category_form" do 
   erb :"user/add_category_form"
 end
+
+MyApp.post "/add_category" do 
+  @category = Category.new
+  @category.category_title = params["category_title"]
+  @category.save
+  @num_of_nominees = params["number_of_nominees"]
+  erb :"user/add_nominees_form"
+end
+
