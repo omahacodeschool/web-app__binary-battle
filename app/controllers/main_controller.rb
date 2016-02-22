390f405743cb284edcabd8e47100131cc618f132
Patrick Clark <patclark1958@gmail.com>
2016-02-22 15:22:32 -0600

    forgot to save a few changes to main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,6 +8,13 @@ MyApp.get "/" do
   # combined document back to the client.
   #
   # "Main" is the gameplay page in this program. 
+
+  @game = Quarterback.new
+  @game.choose_quarterback[1]
+  @game.choose_quarterback[2]
+  #then a miracle happens
+
+
   #
   erb :"main" 
 end
@@ -33,60 +40,3 @@ MyApp.get "/quarterback/create" do
   erb :"success quarterback add"
 end
 
-MyApp.get "/" do 
-  @display_planets = Planet.select_two_planets
-  @new_showdown = Showdown.new
-  @new_showdown.choice_one = @display_planets[0]
-  @new_showdown.choice_two = @display_planets[1]
-  @new_showdown.save
-  @id_of_new_showdown = @new_showdown.id
-  @id_of_last_showdown = @id_of_new_showdown - 1
-  @last_showdown = Showdown.find_by_id(@id_of_last_showdown)
-  @new_showdown.frequency_one = @last_showdown.frequency_one
-  @new_showdown.frequency_two = @last_showdown.frequency_two
-  @new_showdown.save
-  erb :"main/welcome"
-end
-
-MyApp.get "/choice_selected/:num" do
-  @planet_chosen = params[:num]
-  @planet_object = Planet.find_by_planet(@planet_chosen)
-  @current_score_of_planet_chosen = @planet_object.points
-  @new_points = @current_score_of_planet_chosen + 1
-  @planet_object.points = @new_points
-  @planet_object.save
-
-  @last_showdown = Showdown.last
-  if @last_showdown.choice_one == @planet_chosen
-    @current_score_of_freq_one = @last_showdown.frequency_one
-    @new_score_of_freq_one = @current_score_of_freq_one + 1
-    @last_showdown.frequency_one = @new_score_of_freq_one
-    @last_showdown.save
-  elsif @last_showdown.choice_two == @planet_chosen
-    @last_showdown.choice_two == @planet_chosen
-    @current_score_of_freq_two = @last_showdown.frequency_two
-    @new_score_of_freq_two = @current_score_of_freq_two + 1
-    @last_showdown.frequency_two = @new_score_of_freq_two
-    @last_showdown.save
-  end
-  erb :"main/choice_selected"
-end
-
-MyApp.get "/planets_ranking" do
-  @showdowns = Showdown.all
-  @planets = Planet.all
-  @ordered_planets = @planets.order('points DESC')
-  erb :"main/planets_ranking"
-end
-
-
-
-
-
-MyApp.post "/qb_submitted" do
-  x = Quarterback.new
-  x.quarterback = params[:qb_name]
-  x.points = 0
-  x.save
-  erb :"main/qb_submitted"
-end
