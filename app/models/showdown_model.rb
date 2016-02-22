328b8ec6ab12ddd423b4ebc060a750532e2aa28d
Melinda Deinert <melinda.deinert@gmail.com>
2016-02-22 14:23:54 -0600

    Test works with self; need to change lots in controllers to make this work

--- app/models/showdown_model.rb
+++ app/models/showdown_model.rb
@@ -1,19 +1,18 @@
 class Showdown < ActiveRecord::Base
   #returns Nil; adds one "frequency" point to whichever planet was most recently selected
   def give_freq_point_to_chosen_option
-    @last_showdown = Showdown.last
-    @planet_chosen = @last_showdown.winner
-    if @last_showdown.choice_one == @planet_chosen
-      @current_score_of_freq_one = @last_showdown.frequency_one
+    @planet_chosen = self.winner
+    if self.choice_one == @planet_chosen
+      @current_score_of_freq_one = self.frequency_one
       @new_score_of_freq_one = @current_score_of_freq_one + 1
-      @last_showdown.frequency_one = @new_score_of_freq_one
-      @last_showdown.save
-    elsif @last_showdown.choice_two == @planet_chosen
-      @last_showdown.choice_two == @planet_chosen
-      @current_score_of_freq_two = @last_showdown.frequency_two
+      self.frequency_one = @new_score_of_freq_one
+      self.save
+    elsif self.choice_two == @planet_chosen
+      self.choice_two == @planet_chosen
+      @current_score_of_freq_two = self.frequency_two
       @new_score_of_freq_two = @current_score_of_freq_two + 1
-      @last_showdown.frequency_two = @new_score_of_freq_two
-      @last_showdown.save
+      self.frequency_two = @new_score_of_freq_two
+      self.save
     end
   end
 
