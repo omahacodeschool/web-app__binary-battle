ed94b7b02c1bfc96bb068e803c110f283d1d5a1b
Bradley Biehl <bradley.biehl@gmail.com>
2016-02-22 12:05:51 -0600

    working on setting win percentage method

--- app/models/legend.rb
+++ app/models/legend.rb
@@ -4,20 +4,36 @@ class Legend < ActiveRecord::Base
   #   return Legend.all.sample(2)
   # end
 
-  # finds all of the instances in the Results table where the winner_id equals the legend_id
+  # finds all of the instances in the Results table
+  # where the winner_id equals the legend_id
   # returns an Array
   def wins_arr
     return Result.where("winner_id" => self.id)
   end
 
-  # takes 'wins_arr' and counts the instances when winner_id is equal to self.id
+  # takes 'wins_arr' and counts the instances when 
+  # winner_id is equal to self.id
+  #
   # returns an Integer via .length
   def wins_count
     return self.wins_arr.length
   end
 
+  def losses_arr
+    return Result.where("loser_id" => self.id)
+  end
 
-  # this method stores the Legend object 'name' and 'wins'.
+  def losses_count
+    return self.losses_arr.length
+  end
+
+  def win_percentage
+    return wins_count/(wins_count + losses_count)
+  end
+
+  # this method stores the 
+  # Legend object 'name' and 'wins'.
+  #
   # returns a Hash
   def self.wins_hash
     hash = {}
