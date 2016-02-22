1858a51089d98fc90d64a6c34c8df95759628d53
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-22 17:42:06 -0600

    Got the number of votes that a band has received to show up on the /tally page as well. Need to figure out how to sort these in order, high to low.

--- app/controllers/tally_controller.rb
+++ app/controllers/tally_controller.rb
@@ -1,5 +1,6 @@
 #Shows the scores for each artist
 MyApp.get "/tally" do 
+  @bands = Band.all
   @results = Result.all
 
   erb :"tally"
--- app/models/band.rb
+++ app/models/band.rb
@@ -1,3 +1,6 @@
 class Band < ActiveRecord::Base
-
+  def votes
+    votes = Result.where("winner_id" => self.id)
+    return votes.length
+  end
 end
