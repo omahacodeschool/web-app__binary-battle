c4da0cc7847cc190435838c22f6c53d79a52519c
Tyler <tfisher322@gmail.com>
2016-02-22 14:21:49 -0600

    Small update

--- db/define_schema.rb
+++ db/define_schema.rb
@@ -5,6 +5,6 @@ DB.define_column("fighters", "name", "string")
 DB.define_column("fighters", "style", "string")
 
 DB.define_table("results")
-DB.define_column("results","competitor_id_1","integer")
-DB.define_column("results","competitor_id_2","integer")
+DB.define_column("results","fighter_id_1","integer")
+DB.define_column("results","fighter_id_2","integer")
 DB.define_column("results","winner","integer")
