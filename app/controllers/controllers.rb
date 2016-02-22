05027421439818636331ec2139875650ade4bfb4
Courtney Burns <courtneylynnburns@gmail.com>
2016-02-22 13:16:30 -0600

    all pages link and one dwarf can receive points

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -1,15 +1,16 @@
 MyApp.get "/" do
-
   erb :"home_page"
 end
 
 MyApp.get "/submit_vote" do
-
+  @dwarf = Score.find_by_dwarf_name("Dopey")
+  @dwarf.tally += 1
+  @dwarf.save
   erb :"vote_successful_page"
 end
 
 MyApp.get "/view_rankings" do
-
+  @whole_table = Score.all
   erb :"rankings_page"
 end
 
