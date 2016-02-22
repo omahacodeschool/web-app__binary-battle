2a69816937989192c15c1515210ff1acb6838ddb
Ryan Newell <youcatastrophe@gmail.com>
2016-02-22 16:34:07 -0600

    working on methods for determining tie-breaker scenarios

--- app/models/avenger.rb
+++ app/models/avenger.rb
@@ -18,18 +18,42 @@ class Avenger < ActiveRecord::Base
     rankings_hash
   end
 
-  def find_ties
+  #Determines if there are two or more Avengers with the same vote count.
+  #
+  #Returns Nil if there are no ties
+  #
+  #Otherwise, returns nested array containing the name and value of the vote count of each tied Avenger.
+
+  def self.find_ties
     tied_heroes = []
     x = Avenger.rank
-    a = x.group_by{|k,v| v}
+    a = x.group_by {|k, v| v}
     a.each do |k, v|
       if v.length > 1
         tied_heroes << v
+        tied_heroes
       else
         nil
       end 
     end
   end
+
+  #Identifies the names of the Avengers that tied.
+  #
+  #Returns 
+  def tied_hero_names
+    if Avenger.find_ties != nil
+      first_hero_tied = Avenger.find_ties[0][0]
+      second_hero_tied = Avenger.find_ties[0][1]
+      hero_1_name = first_hero_tied[0].join
+      hero_2_name = second_hero_tied[0].join
+    end
+  end
+
+  def tie_breaker
+    
+    @applicable_showdowns = Showdown.where("winner_id" =>)
+  end 
 
 
 end
--- app/models/showdown.rb
+++ app/models/showdown.rb
@@ -10,7 +10,7 @@ class Showdown < ActiveRecord::Base
   #
   #returns hero_1_id if hero_1_id is not also the winner_id
   #returns hero_2_id if hero_1_id  *is* also the winner_id
-  
+
   def get_loser
     if self.hero_1_id != self.winner_id
       self.hero_1_id
@@ -18,6 +18,7 @@ class Showdown < ActiveRecord::Base
       self.hero_2_id
     end
   end
+
 
 
   #a method a found online, but I'm not sure how to implement it. Probaby won't use this.
