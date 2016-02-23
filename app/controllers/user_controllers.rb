6ffff9f544a9fdc7576709e95ad52040c54608a6
Tray Harrington <traylon.harrington@gmail.com>
2016-02-22 18:48:17 -0600

    Add Nominees Form Working

--- app/controllers/user_controllers.rb
+++ app/controllers/user_controllers.rb
@@ -2,11 +2,13 @@ MyApp.get "/add_category_form" do
   erb :"user/add_category_form"
 end
 
-MyApp.post "/add_category" do 
+MyApp.post "/add_category" do
   @category = Category.new
   @category.category_title = params["category_title"]
   @category.save
   @num_of_nominees = params["number_of_nominees"]
+  @categories = Category.all
+  @count = 1
   erb :"user/add_nominees_form"
 end
 
