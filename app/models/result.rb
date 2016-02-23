dc70936fb3c81bfc5d9a4106db4128f852f73ca2
Thorin Butler <thorin.butler@gmail.com>
2016-02-23 10:38:32 -0600

    Forgot to commit for a moment there. I managed to sort out how to get the array I produced of results to get processed for frequency.  This generates a hash and then you can sort that for by values to get a ranking.  Then it was a matter of writing methods to display my IDs as their associated names.  So far, it seems to be working...ish.

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,35 @@
 class Result < ActiveRecord::Base
+
+  def comp_1_name
+    comp = Competitor.find_by_id(self.comp_id_1)
+    name = comp.name
+    return name
+  end
+
+  def comp_2_name
+    comp = Competitor.find_by_id(self.comp_id_2)
+    name = comp.name
+    return name
+  end
+
+  def winner_name
+    comp = Competitor.find_by_id(self.winner)
+    name = comp.name
+    return name
+  end
+
+  def rankings(counter)
+    result = Hash.new(0)
+    counter.each {|comp| result[comp] += 1 }
+    key_order = Hash[result.sort_by{|k, v| v}.reverse]
+    rankings = key_order.keys.each {|key| key}
+    return rankings
+  end
+
+  def competitor_name(competitor_id)
+    comp = Competitor.find_by_id(competitor_id)
+    name = comp.name
+    return name
+  end
+
 end
