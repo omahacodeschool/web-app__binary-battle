8a58bdf6afeb1a9a97a11f04bf33dc8d1fdec3db
Jillian Waggoner <jillianwaggoner@icloud.com>
2016-02-23 08:58:43 -0600

    results page works now

--- app/controllers/tally_controller.rb
+++ app/controllers/tally_controller.rb
@@ -1,6 +1,6 @@
 #Shows the scores for each artist
 MyApp.get "/tally" do 
-  @bands = Band.all
+  @bands = Band.band_ranks
   @results = Result.all
 
   erb :"tally"
--- app/models/band.rb
+++ app/models/band.rb
@@ -3,4 +3,12 @@ class Band < ActiveRecord::Base
     votes = Result.where("winner_id" => self.id)
     return votes.length
   end
+
+  def self.band_ranks
+    sorted_bands = {}
+    Band.all.each do |b|
+      sorted_bands[b.name] = b.votes
+    end
+    return sorted_bands.sort_by {|_key, value| value}.reverse.to_h
+  end
 end
