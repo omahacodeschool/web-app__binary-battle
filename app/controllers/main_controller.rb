e57b467a30236c79a3ba4c00150d6cc858158e22
Meagan Jungman <meagan.jungman@gmail.com>
2016-02-23 08:39:22 -0600

    can lock nominees and categories

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,3 +2,25 @@ MyApp.get "/" do
 
   erb :"/main"
 end
+
+MyApp.get "/admin/admin_tools" do
+  @categories = Category.all
+  @nominees = Nominee.all
+  @pools = Pool.all
+  @results = Result.all
+  erb :"/admin/admin_tools"
+end
+
+MyApp.post "/admin/admin_tools/category/locked/confirmation" do
+  @category = Category.find_by_id(params[:lock_category_dropdown])
+  @category.locked = true
+  @category.save
+  redirect :"/admin/admin_tools"
+end
+
+MyApp.post "/admin/admin_tools/nominee/locked/confirmation" do
+  @nominee = Nominee.find_by_id(params[:lock_nominees_dropdown])
+  @nominee.locked = true
+  @nominee.save
+  redirect :"/admin/admin_tools"
+end
