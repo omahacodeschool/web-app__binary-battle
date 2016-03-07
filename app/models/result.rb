58f62647358a2b4a37f4f7eb299d7bf906788c64
Thorin Butler <thorin.butler@gmail.com>
2016-03-06 22:09:23 -0600

    just saw a typo

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,23 +1,35 @@
 class Result < ActiveRecord::Base
 
+  #Returns a competitor's name in lieu of their ID
+  #
+  #Returns a String
   def comp_1_name
     comp = Competitor.find_by_id(self.comp_id_1)
     name = comp.name
     return name
   end
 
+  #Returns a competitor's name in lieu of their ID
+  #
+  #Returns a String
   def comp_2_name
     comp = Competitor.find_by_id(self.comp_id_2)
     name = comp.name
     return name
   end
 
+  #Returns the winning competitor's name in lieu of their ID
+  #
+  #Returns a String
   def winner_name
     comp = Competitor.find_by_id(self.winner)
     name = comp.name
     return name
   end
 
+  #Takes an array of wins, sorts by frequency, and returns array in descending #order
+  #
+  #Returns an Array
   def rankings(counter)
     result = Hash.new(0)
     counter.each {|comp| result[comp] += 1 }
@@ -26,6 +38,9 @@ class Result < ActiveRecord::Base
     return rankings
   end
 
+  #Returns a competitor's name in lieu of their ID
+  #
+  #Returns a String
   def competitor_name(competitor_id)
     comp = Competitor.find_by_id(competitor_id)
     name = comp.name
